219336cf9e98f45874bc7cc9be070f93d4caf069
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 04:57:07 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,28 +3,28 @@ require "yaml"
 
 def load_library(path)
   library = {
-    "get_meaning": {},
-    "get_emoticon": {}
+    "get_meaning" => {},
+    "get_emoticon"=> {}
   } 
   YAML.load_file(path).each do |emotion, emoticons|
     eng = emoticons[0]
     jpn = emoticons[1]
-    library[:"get_emoticon"][eng] = jpn
-    library[:"get_meaning"][jpn] = emotion
+    library["get_emoticon"][eng] = jpn
+    library["get_meaning"][jpn] = emotion
   end
   library
 end
 
 def get_japanese_emoticon (path, emoticon)
   library = load_library(path)
-  jpn_emoticon = library[:"get_emoticon"][emoticon]
+  jpn_emoticon = library["get_emoticon"][emoticon]
   return jpn_emoticon if jpn_emoticon
   "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning (path, emoticon)
   library = load_library(path)
-  emotion = library[:"get_meaning"][emoticon]
+  emotion = library["get_meaning"][emoticon]
   return emotion if emotion
   "Sorry, that emoticon was not found"
 end
