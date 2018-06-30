e09b2e3c2ac6e5fe1a8dc13c7b73b71b61b3fe3b
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 04:56:12 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,22 +9,22 @@ def load_library(path)
   YAML.load_file(path).each do |emotion, emoticons|
     eng = emoticons[0]
     jpn = emoticons[1]
-    library["get_emoticon"][eng] = jpn
-    library["get_meaning"][jpn] = emotion
+    library[:"get_emoticon"][eng] = jpn
+    library[:"get_meaning"][jpn] = emotion
   end
   library
 end
 
 def get_japanese_emoticon (path, emoticon)
   library = load_library(path)
-  jpn_emoticon = library["get_emoticon"][emoticon]
-  return jpn_emoticon if jpn_emoticon !== nil
+  jpn_emoticon = library[:"get_emoticon"][emoticon]
+  return jpn_emoticon if jpn_emoticon
   "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning (path, emoticon)
   library = load_library(path)
-  emotion = library["get_meaning"][emoticon]
-  return emotion if emotion !== nil
+  emotion = library[:"get_meaning"][emoticon]
+  return emotion if emotion
   "Sorry, that emoticon was not found"
 end
