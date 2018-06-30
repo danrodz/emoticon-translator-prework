537816b6a3eccdc0ecab5cb9770f449aa824dc4d
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 04:42:45 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,13 @@ def load_library
 end
 
 def get_japanese_emoticon
-  # code goes here
+  library = load_library(file_path)
+  result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
